150e266fdcdffe7b81cd7d6c2f692356577c0f6b
Ivanov Ilya <gergen892@gmail.com>
2024-04-24 18:19:00 +0500

    added .xcodeproj and .xcworkspace

--- DZ18/ViewController.swift
+++ DZ18/ViewController.swift
@@ -10,124 +10,128 @@ import SnapKit
 import Alamofire
 
 class ViewController: UIViewController {
+  
+  var userInput: String?
+  var urlTypeString = ""
+  var url: URL?
+  let mainQueue = DispatchQueue.main
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    setupLinkTextView()
+    setupInputTextField()
+    setupAlamofireButton()
+    setupUrlSessionButton()
     
-    var userInput: String?
-    var urlString = ""
-    var url: URL?
-    let mainQueue = DispatchQueue.main
+  }
+  
+  @IBOutlet weak var urlSessionButtonOutlet: UIButton!
+  @IBOutlet weak var alamofireButtonOutlet: UIButton!
+  @IBOutlet weak var inputTextFieldOutlet: UITextField!
+  @IBOutlet weak var linkTextViewOutlet: UITextView!
+  
+  @IBAction func inputTextField(_ sender: Any) {
+  }
+  @IBAction func alamofireButton(_ sender: Any) {
+    networkRequestUsingURLSession()
+  }
+  @IBAction func urlSessionButton(_ sender: Any) {
+    networkRequestUsingURLSession()
+  }
+  
+  //MARK: Функция реализации сетевого запроса через URLSession
+  private func networkRequestUsingURLSession(){
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        setupLinkTextView()
-        setupInputTextField()
-        setupAlamofireButton()
-        setupUrlSessionButton()
-        
-    }
+    var requestForSessionButton = URLRequest(url: createdURL() )
+    requestForSessionButton.httpMethod = "GET"
+    requestForSessionButton.allHTTPHeaderFields = [
+      "X-API-KEY" : "69352346-76a6-4ed5-a233-346dc7cc7127",
+      "Content-Type": "application/json"]
     
-    @IBOutlet weak var urlSessionButtonOutlet: UIButton!
-    @IBOutlet weak var alamofireButtonOutlet: UIButton!
-    @IBOutlet weak var inputTextFieldOutlet: UITextField!
-    @IBOutlet weak var linkTextViewOutlet: UITextView!
-    
-    @IBAction func inputTextField(_ sender: Any) {
-    }
-    @IBAction func alamofireButton(_ sender: Any) {
-        createdAlamofireLink()
-    }
-    @IBAction func urlSessionButton(_ sender: Any) {
-        createdUrlSessionLink()
-    }
-    
-    func setupUrlSessionButton(){
-        urlSessionButtonOutlet.layer.cornerRadius = 15
-        urlSessionButtonOutlet.snp.makeConstraints { make in
-            make.width.equalTo(150)
-            make.height.equalTo(40)
-            make.top.equalToSuperview().inset(180)
-            make.left.equalToSuperview().inset(20)
+    let task = URLSession.shared.dataTask(with: requestForSessionButton, completionHandler: { data,response, error in
+      if let error = error {
+        print(error)
+      } else if let data = data, let _ = try? JSONSerialization.jsonObject(with:data, options: []) {
+        let convertedString = String(data: data, encoding: .utf8)
+        self.mainQueue.async {
+          self.linkTextViewOutlet.text = convertedString
         }
-    }
-    func setupAlamofireButton(){
-        alamofireButtonOutlet.layer.cornerRadius = 15
-        alamofireButtonOutlet.snp.makeConstraints { make in
-            make.width.equalTo(150)
-            make.height.equalTo(40)
-            make.top.equalToSuperview().inset(180)
-            make.right.equalToSuperview().inset(20)
-        }
-    }
-    func setupLinkTextView(){
-        linkTextViewOutlet.layer.cornerRadius = 15
-        linkTextViewOutlet.font = .systemFont(ofSize: 12)
-        linkTextViewOutlet.snp.makeConstraints { make in
-            make.height.equalTo(550)
-            make.bottom.equalToSuperview().inset(60)
-            make.right.equalToSuperview().inset(20)
-            make.left.equalToSuperview().inset(20)
-        }
-    }
-    func setupInputTextField(){
-        inputTextFieldOutlet.font = .systemFont(ofSize: 18)
-        inputTextFieldOutlet.layer.cornerRadius = 15
-        inputTextFieldOutlet.snp.makeConstraints { make in
-            make.height.equalTo(60)
-            make.top.equalToSuperview().inset(100)
-            make.right.equalToSuperview().inset(20)
-            make.left.equalToSuperview().inset(20)
-        }
-    }
+        print(convertedString as Any)
+      }
+    })
+    task.resume()
+  }
+  
+  //MARK: Функция реализации сетевого запроса через Alamofire
+  private func networkRequestUsingAlamofire(){
     
-    func createdUrlSessionLink(){
-        
-        var requestForSessionButton = URLRequest(url: createdURL() )
-        requestForSessionButton.httpMethod = "GET"
-        requestForSessionButton.allHTTPHeaderFields = [
-            "X-API-KEY" : "69352346-76a6-4ed5-a233-346dc7cc7127",
-            "Content-Type": "application/json"]
-        
-        let task = URLSession.shared.dataTask(with: requestForSessionButton, completionHandler: { data,response, error in
-            if let error = error {
-                print(error)
-            }
-            else if let data = data, let _ = try? JSONSerialization.jsonObject(with:data, options: []) {
-                let convertedString = String(data: data, encoding: .utf8)
-                self.mainQueue.async {
-                    self.linkTextViewOutlet.text = convertedString
-                }
-                print(convertedString as Any)
-            }
-        })
-        task.resume()
+    AF.request(createdURL(),
+               headers: [
+                "X-API-KEY" : "69352346-76a6-4ed5-a233-346dc7cc7127",
+                "Content-Type": "application/json"]
+    ).response { response in
+      guard response.error == nil else{
+        return}
+      guard let data = response.data else{
+        return}
+      let convertedString = String(data: data, encoding: .utf8)
+      self.linkTextViewOutlet.text = convertedString
+      debugPrint(convertedString ?? "")
     }
+  }
+  
+  //MARK: Функция для создания URL
+  private func createdURL()-> URL{
     
-    func createdAlamofireLink(){
-        let filmInstance = FilmInstance(keyword: userInput ?? "", pagesCount: 0)
-        
-        AF.request(url!,
-                   headers: [
-                    "X-API-KEY" : "69352346-76a6-4ed5-a233-346dc7cc7127",
-                    "Content-Type": "application/json"]
-        ).response { response in
-            guard response.error != nil else{return}
-            guard let data = response.data else{return}
-            let convertedString = String(data: data, encoding: .utf8)
-            self.mainQueue.async {
-                self.linkTextViewOutlet.text = convertedString
-            }
-            debugPrint(convertedString ?? "")
-            print(response.response?.statusCode)
-        }
-    }
+    userInput = inputTextFieldOutlet.text
+    urlTypeString = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
+    urlTypeString += userInput ?? ""
+    url = URL(string: urlTypeString)
     
-    func createdURL()-> URL{
-        
-        userInput = inputTextFieldOutlet.text
-        urlString = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
-        urlString += userInput ?? ""
-        url = URL(string: urlString)
-        
-        return url!
+    return url!
+  }
+  
+  //MARK: Функция для настройки кнопки URLSession
+  private func setupUrlSessionButton(){
+    urlSessionButtonOutlet.layer.cornerRadius = 15
+    urlSessionButtonOutlet.snp.makeConstraints { make in
+      make.width.equalTo(150)
+      make.height.equalTo(40)
+      make.top.equalToSuperview().inset(180)
+      make.left.equalToSuperview().inset(20)
     }
+  }
+  //MARK: Функция для настройки кнопки Alamofire
+  private func setupAlamofireButton(){
+    alamofireButtonOutlet.layer.cornerRadius = 15
+    alamofireButtonOutlet.snp.makeConstraints { make in
+      make.width.equalTo(150)
+      make.height.equalTo(40)
+      make.top.equalToSuperview().inset(180)
+      make.right.equalToSuperview().inset(20)
+    }
+  }
+  //MARK: Функция для настройки кнопки TextView
+  private func setupLinkTextView(){
+    linkTextViewOutlet.layer.cornerRadius = 15
+    linkTextViewOutlet.font = .systemFont(ofSize: 12)
+    linkTextViewOutlet.snp.makeConstraints { make in
+      make.height.equalTo(550)
+      make.bottom.equalToSuperview().inset(60)
+      make.right.equalToSuperview().inset(20)
+      make.left.equalToSuperview().inset(20)
+    }
+  }
+  //MARK: Функция для настройки кнопки TextField
+  private func setupInputTextField(){
+    inputTextFieldOutlet.font = .systemFont(ofSize: 18)
+    inputTextFieldOutlet.layer.cornerRadius = 15
+    inputTextFieldOutlet.snp.makeConstraints { make in
+      make.height.equalTo(60)
+      make.top.equalToSuperview().inset(100)
+      make.right.equalToSuperview().inset(20)
+      make.left.equalToSuperview().inset(20)
+    }
+  }
 }
